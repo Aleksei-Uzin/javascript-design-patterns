29e36a0156b3a6850bbb891b9b98208ce212d1d5
Aleksei-Uzin <aleksei.uzin@gmail.com>
2026-02-17 14:27:45 +0300

    for...in loop: examples and testing

--- src/JavaScript-Fundamentals/Loops/forInLoop.test.js
+++ src/JavaScript-Fundamentals/Loops/forInLoop.test.js
@@ -1,4 +1,37 @@
-describe('for in loop', () => {
+describe('for...in loop', () => {
+  it('Should iterate over enumerable properties and ignore non-enumerable properties', () => {
+    const obj = { a: 1, b: 2, c: 3 };
+    const keys = [];
+
+    // toString not enumerable
+    expect(obj.propertyIsEnumerable('toString')).toBe(false);
+
+    // eslint-disable-next-line guard-for-in
+    for (const key in obj) {
+      keys.push(key);
+    }
+
+    expect(keys).toEqual(['a', 'b', 'c']);
+  });
+
+  it('Should exclude inherited enumerable properties', () => {
+    const user = { name: 'Aleksei', active: true };
+    const obj = Object.create(user, {
+      a: { value: 1, enumerable: true },
+      b: { value: 2, enumerable: true },
+      c: { value: 3, enumerable: true },
+    });
+    const keys = [];
+
+    for (const key in obj) {
+      if (Object.hasOwn(obj, key)) {
+        keys.push(key);
+      }
+    }
+
+    expect(keys).toEqual(['a', 'b', 'c']);
+  });
+
   it('Allows arbitrary expressions as the loop variable', () => {
     const obj = { a: 1, b: 2, c: 3 };
     const keys = [];
