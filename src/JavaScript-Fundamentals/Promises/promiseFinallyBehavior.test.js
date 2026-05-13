3efcc741357fb3f35a71bf25dd267294033ae749
Aleksei-Uzin <aleksei.uzin@gmail.com>
2026-05-13 21:54:12 +0300

    test: update promiseFinallyBehavior tests

--- src/JavaScript-Fundamentals/Promises/promiseFinallyBehavior.test.js
+++ src/JavaScript-Fundamentals/Promises/promiseFinallyBehavior.test.js
@@ -1,6 +1,3 @@
-/* eslint-disable prefer-promise-reject-errors */
-/* eslint-disable no-throw-literal */
-
 describe('Promise.finally()', () => {
   it('Returns a new pending promise regardless of current promise status', () => {
     const settled = Promise.resolve('value');
@@ -31,40 +28,42 @@ describe('Promise.finally()', () => {
   describe('Demonstrates ignore callback return value', () => {
     it('Passes original fulfillment value or rejection reason when callback returns a plain value', async () => {
       const promiseA = Promise.resolve('value').finally(() => 'ignored');
-      const promiseB = Promise.reject('Rejected').finally(() => 'ignored');
+      const promiseB = Promise.reject(new Error('Rejected')).finally(() => 'ignored');
 
       await expect(promiseA).resolves.toBe('value');
-      await expect(promiseB).rejects.toBe('Rejected');
+      await expect(promiseB).rejects.toThrow('Rejected');
     });
 
     it('Passes original fulfillment value or rejection reason when callback returns a fulfilled promise', async () => {
+      const rejected = Promise.reject(new Error('Rejected'));
       const promiseA = Promise.resolve('value').finally(() => Promise.resolve('ignored'));
-      const promiseB = Promise.reject('Rejected').finally(() => Promise.resolve('ignored'));
+      const promiseB = rejected.finally(() => Promise.resolve('ignored'));
 
       await expect(promiseA).resolves.toBe('value');
-      await expect(promiseB).rejects.toBe('Rejected');
+      await expect(promiseB).rejects.toThrow('Rejected');
     });
   });
 
   describe('Demonstrates override original value', () => {
     it('Rejects with thrown error when callback throws', async () => {
       const promiseA = Promise.resolve(55).finally(() => {
-        throw 'Interrupted';
+        throw new Error('Rejected');
       });
-      const promiseB = Promise.reject(55).finally(() => {
-        throw 'Interrupted';
+      const promiseB = Promise.reject(new Error(55)).finally(() => {
+        throw new Error('Rejected');
       });
 
-      await expect(promiseA).rejects.toBe('Interrupted');
-      await expect(promiseB).rejects.toBe('Interrupted');
+      await expect(promiseA).rejects.toThrow('Rejected');
+      await expect(promiseB).rejects.toThrow('Rejected');
     });
 
     it('Rejects with reason when callback returns rejected promise', async () => {
-      const promiseA = Promise.resolve(55).finally(() => Promise.reject('Rejected'));
-      const promiseB = Promise.reject(55).finally(() => Promise.reject('Rejected'));
+      const rejected = Promise.reject(new Error(55));
+      const promiseA = Promise.resolve(55).finally(() => Promise.reject(new Error('Rejected')));
+      const promiseB = rejected.finally(() => Promise.reject(new Error('Rejected')));
 
-      await expect(promiseA).rejects.toBe('Rejected');
-      await expect(promiseB).rejects.toBe('Rejected');
+      await expect(promiseA).rejects.toThrow('Rejected');
+      await expect(promiseB).rejects.toThrow('Rejected');
     });
   });
 });
