ae7d7a7f21b2fb6021e5342831b0a1463e202934
Aleksei-Uzin <aleksei.uzin@gmail.com>
2026-06-02 22:04:43 +0300

    chore: update ESLint config and gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { 'jest/globals': true, es2022: true },
+  env: { 'jest/globals': true, es2022: true, browser: true },
   extends: 'airbnb-base',
   plugins: ['jest'],
   parserOptions: {
@@ -12,9 +12,11 @@ module.exports = {
     'func-names': ['error', 'as-needed'],
     'generator-star-spacing': 'off',
     'no-extend-native': 'off',
+    'no-param-reassign': ['error', { props: false }],
     'no-plusplus': 'off',
     'no-prototype-builtins': 'off',
     'no-restricted-syntax': ['error', 'WithStatement'],
+    'import/extensions': ['error', 'always', { ignorePackages: true }],
     'no-unreachable-loop': 'off',
     'no-useless-escape': 'off',
     'object-curly-newline': [
